458ad00646e003baec7d1cd89487535df795e414
Van Nam <vannam272008@gmail.com>
2022-10-17 11:33:02 +0700

    Test Connect DB 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookAPIController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books',[
+    BookAPIController::class,"index"
+]);
